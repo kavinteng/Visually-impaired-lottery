3a61f3893cb07b97a6790b8b998b3c4db0f201fb
kavinteng <76694895+kavinteng@users.noreply.github.com>
2021-03-28 22:05:57 +0700

    Add files via upload

---
+++ database/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import lottery
+
+# Register your models here.
+admin.site.register(lottery)
---
+++ database/code.py
@@ -0,0 +1,6 @@
+from .test2 import mydata
+
+def test():
+    num = mydata()
+
+    return num
